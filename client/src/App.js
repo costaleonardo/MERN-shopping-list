0fd4a13ef07f36c4f9134de20d48f45029c45d7e
costaleonardo <costaleonardo97@gmail.com>
2018-09-05 05:28:49 -0400

    Add <ShoppingList /> componentt.

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import AppNav from './components/Navbar';
+import ShoppingList from './components/ShoppingList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <AppNav />
+        <ShoppingList />
       </div>
     );
   }

f840262d76c20b619b1368929dc14b1cd21af814
costaleonardo <costaleonardo97@gmail.com>
2018-09-05 04:22:41 -0400

    Connect to MongoDB with mongoose.

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://dbuser:dbuser123@ds245772.mlab.com:45772/mern-shopping-list'
+};
--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// MongoDB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
+
 // Body parser middleware
 app.use(bodyParser.json());
 
